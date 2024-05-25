3320ba42a99df10d94ca7c28b162b5bc53550007
notnotmax <156508404+notnotmax@users.noreply.github.com>
2024-05-25 16:17:41 +0800

    Add player movement

--- scenes/levels/Player.gd
+++ scenes/levels/Player.gd
@@ -1,23 +1,33 @@
 extends Area2D
 signal hit
 
-var player_size = 30  # radius in pixels
-
+var player_sprite_move_up = load("res://assets/ship.png")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	position.x = 50
-
+	# Lock the cursor to the center of the screen and hide it
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	position.x = 1280 / 2
+	position.y = 720 / 2
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	position.y = clamp(
-		get_viewport().get_mouse_position().y,
-		player_size / 2,
-		get_viewport_rect().size.y - player_size / 2
-		)
+	pass
 
-
-func _on_body_entered(body):
-	hit.emit()
-	$Hurtbox.set_deferred("disabled", true)
+func _input(event):
+	# Player movement
+	if event is InputEventMouseMotion:
+		position += event.relative
+		position.x = clamp(position.x, 16, get_viewport_rect().size.x - 24)
+		position.y = clamp(position.y, 16, get_viewport_rect().size.y - 16)
+		# change movement sprite
+		if event.relative.y > 4:
+			$Sprite.frame = 1
+		elif event.relative.y > 0:
+			$Sprite.frame = 3
+		elif event.relative.y == 0:
+			$Sprite.frame = 5
+		elif event.relative.y > -2:
+			$Sprite.frame = 7
+		else:
+			$Sprite.frame = 9
