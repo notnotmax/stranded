428f9e92485fe816b47da73b5300c15529657699
notnotmax <156508404+notnotmax@users.noreply.github.com>
2024-05-27 15:05:41 +0800

    Make level completable

--- scenes/levels/empty_demo.gd
+++ scenes/levels/empty_demo.gd
@@ -1,16 +1,12 @@
 extends Control
 signal level_completed
 
-var level_duration = 150
-var progress: float = 0
+var level_duration = 3 * 60
+var elapsed_frames = 0
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var progress_bar = $ProgressBar
-	var progress_label = $ProgressBar/ProgressLabel
-	progress += 1
-	progress_bar.value = (progress * 100) / level_duration
-	progress_label.text = str(progress_bar.value) + "%"
-	if (progress == level_duration):
+	elapsed_frames += 1
+	if elapsed_frames >= level_duration:
 		level_completed.emit()
