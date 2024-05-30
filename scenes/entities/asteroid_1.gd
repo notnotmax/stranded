746f1cc4d9567c6827fcba7a8779a67bcb15dfa9
notnotmax <156508404+notnotmax@users.noreply.github.com>
2024-05-30 10:55:38 +0800

    Added basic asteroid
    easily destroyable environmental obstacle

---
+++ scenes/entities/asteroid_1.gd
@@ -0,0 +1,23 @@
+extends Area2D
+
+var alive = true
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AnimatedSprite2D.play("default")
+
+
+func _on_area_entered(_area):
+	if alive:
+		alive = false
+		set_collision_layer_value(6, false)
+		$AnimatedSprite2D.play("death")
+
+
+func _on_animated_sprite_2d_animation_finished():
+	if !alive:
+		queue_free()
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
