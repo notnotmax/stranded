4f89b8cb340c926533793c6e80d6c91d8c2affe7
notnotmax <156508404+notnotmax@users.noreply.github.com>
2024-05-30 11:51:07 +0800

    Add more asteroid types

---
+++ scenes/entities/asteroid_4.gd
@@ -0,0 +1,37 @@
+extends Area2D
+
+var alive = true
+var health = 100
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AnimatedSprite2D.play("default")
+
+
+# destroy the player on impact
+func _on_area_entered(area):
+	if alive:
+		if area.get_collision_layer_value(2):
+			area.die()
+
+
+func _on_animated_sprite_2d_animation_finished():
+	if !alive:
+		queue_free()
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
+
+
+func damage(dmg):
+	health -= dmg
+	if health <= 0:
+		die()
+
+
+func die():
+	if alive:
+		alive = false
+		set_collision_layer_value(6, false)
+		$AnimatedSprite2D.play("death")
