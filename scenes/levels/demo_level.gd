53d6dc15cce6a371c2d2def95ad9671c08333bce
notnotmax <156508404+notnotmax@users.noreply.github.com>
2024-06-01 13:59:53 +0800

    Dim background brightness for visibility

--- scenes/levels/demo_level.gd
+++ scenes/levels/demo_level.gd
@@ -6,20 +6,20 @@ signal level_completed
 @export var asteroid3: PackedScene
 @export var asteroid4: PackedScene
 
-# level takes (level_duration / 60) seconds to complete
-var level_duration = 30 * 60
-var elapsed_frames = 0
 
 func _ready():
-	$AsteroidTimer.start()
+	$StartTimer.start()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta):
-	elapsed_frames += 1
-	if elapsed_frames >= level_duration:
-		level_completed.emit()
+	$LevelName.modulate = lerp($LevelName.modulate, Color(1,1,1,0), 0.05)
 
+
+func _on_start_timer_timeout():
+	$AsteroidTimer.start()
+	$LevelTimer.start()
+	
 
 func _on_asteroid_timer_timeout():
 	spawn_asteroid_1()
@@ -29,6 +29,13 @@ func _on_asteroid_timer_timeout():
 	
 	if randf() < 0.5:
 		spawn_asteroid_3()
+	
+	if randf() < 0.25:
+		spawn_asteroid_4()
+
+
+func _on_level_timer_timeout():
+	level_completed.emit()
 
 
 func spawn_asteroid_1():
@@ -65,6 +72,19 @@ func spawn_asteroid_3():
 	
 	asteroid.position = location.position
 	var velocity = Vector2(randf_range(0.5,1), 0)
+	asteroid.velocity = velocity
+	
+	add_child(asteroid)
+
+
+func spawn_asteroid_4():
+	var asteroid = asteroid4.instantiate()
+	
+	var location = $Path2D/PathFollow2D
+	location.progress_ratio = randf()
+	
+	asteroid.position = location.position
+	var velocity = Vector2(randf_range(0.2,1), 0)
 	asteroid.velocity = velocity
 	
 	add_child(asteroid)
