09d0984cb4956fea17f30e9f50e876d136f0e765
notnotmax <156508404+notnotmax@users.noreply.github.com>
2024-06-14 23:04:04 +0800

    Add enemy: deathbomber
    explodes upon death

---
+++ scenes/entities/enemies/deathbomber.gd
@@ -0,0 +1,26 @@
+extends Enemy
+class_name Deathbomber
+
+@export var Bullet: PackedScene
+
+func init_deathbomber(p_position: Vector2 = Vector2(0, 0),
+	p_target: Node = Node.new()):
+	super.init_enemy(p_position, p_target)
+
+
+func die():
+	if alive:
+		call_deferred("on_death")
+		super.die()
+
+
+func on_death():
+	var angle = Vector2.RIGHT
+	var incr = PI / 6
+	for i in range(12):
+		var bullet = Bullet.instantiate()
+		bullet.init_enemy_attack(
+			self.global_position, 5, angle
+		)
+		get_tree().current_scene.add_child(bullet)
+		angle = angle.rotated(incr)
